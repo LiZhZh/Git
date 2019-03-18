cb304d6615e8c084ae54d61841661323b12e887c
李忠政 <lizhongzheng@qiqugames.com>
2019-03-18 09:25:14 +0800

    table相同

--- test/helloworld.lua
+++ test/helloworld.lua
@@ -9,5 +9,20 @@ end
 
 print("分支合并实验2")
 
-function fix()
+function IsSame(tab1,tab2)
+	if(type(tab1)~=type(tab2)) then return false end
+	local len=0
+	for k,v in pairs(tab1) do
+		if(type(v)=="table") then
+			return IsSame(v,tab[k])
+		else
+			return v==tab2[k]
+		end
+		len=len+1
+	end
+	for _,v in pairs(tab2) do
+		len=len-1
+		if(len<0) then return false end
+	end
+	return true
 end
